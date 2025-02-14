09b6a392221027c877717c202901e76f74dd133c
Rafael Yuiti Kondo Bueno <rafaelkondobueno@gmail.com>
2025-02-13 22:46:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -26,10 +26,6 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Rotas
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 
 // Iniciar o servidor
 app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000"));
